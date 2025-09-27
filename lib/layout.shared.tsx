8789dfbc283ae4d1a6d9141cc55605c538c3e1f5
ahadsheikh1814 <ahadsheikh1814@outlook.com>
2025-09-27 19:49:15 +0600

    Add the introduction docs

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -7,10 +7,10 @@ export function baseOptions(): BaseLayoutProps {
     nav: {
       title: (
         <div className='flex justify-center items-center'>
-          <h1 className='h-10 w-10 text-secondary-foreground'>
+          <h1 className='h-10 w-10 dark:text-white text-neutral-800'>
             <IconLayoutDashboardFilled/>
           </h1>
-          <h1 className='text-primary-foreground hidden md:block text-lg font-semibold tracking-tight -mt-4'>AHs Lab</h1>
+          <h1 className='dark:text-white text-neutral-800 hidden md:block text-lg font-semibold tracking-tight -mt-4'>AHs Lab</h1>
         </div>
       ),
       transparentMode: 'top',
