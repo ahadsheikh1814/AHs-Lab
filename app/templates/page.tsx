3dc22448e17e0315957064a19bf90da033583569
ahadsheikh1814 <ahadsheikh1814@outlook.com>
2025-10-25 19:03:03 +0600

    add new templeate

--- app/templates/page.tsx
+++ app/templates/page.tsx
@@ -6,7 +6,7 @@ const Page = () => {
   return (
     <div>
       <TempleteHeading />
-      <div className="mt-10">
+      <div className="my-10 flex flex-col gap-5">
         <TempleteCard
           title="Slate"
           description="Showcase your studio’s portfolio with a clean, modern, and animated design."
@@ -15,6 +15,15 @@ const Page = () => {
           ImageUrl1="/img/slate-1.webp"
           ImageUrl2="/img/slate-2.webp"
           ImageUrl3="/img/slate-3.webp"
+        />
+        <TempleteCard
+          title="Minimal Protfolio"
+          description="Showcase your Skils with a clean, modern, and Minimal design."
+          previewUrl="https://ahadsheikh.vercel.app/"
+          GithubUrl="https://github.com/ahadsheikh1814/Ahs"
+          ImageUrl1="/img/protfolio-1.webp"
+          ImageUrl2="/img/protfolio-2.webp"
+          ImageUrl3="/img/protfolio-3.webp"
         />
       </div>
     </div>
--- resistry/blocks/navigations/Navigation_03.tsx
+++ resistry/blocks/navigations/Navigation_03.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Navigation_03 = () => {
   return (
-    <div>
-      Hello Navigation_03
-    </div>
-  )
-}
+    <header className="w-full max-w-5xl rounded-2xl mt-5 dark:bg-neutral-800 bg-neutral-100 h-14 mx-auto dark:border-neutral-700 border backdrop-blur-sm border-neutral-200 flex items-center justify-between px-5">
+      <Logo/>
+    </header>
+  );
+};
 
-export default Navigation_03
+export default Navigation_03;
+
+const Logo =()=>{
+  return(
+    <div>
+      <h1 className="text-xl font-bold font-mono">AHs-Lab</h1>
+    </div>
+  );
+}
